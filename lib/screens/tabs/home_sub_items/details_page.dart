045746177cab49d0f2bb4343acf3ce628c9c5443
Ahmed <ahmedm.81992@gmail.com>
2024-04-10 14:46:45 +0200

    Merged Dr.Kareems's work with old into Dev

--- lib/screens/tabs/home_sub_items/details_page.dart
+++ lib/screens/tabs/home_sub_items/details_page.dart
@@ -21,11 +21,8 @@ class _DetailsPageState extends State<DetailsPage> {
   @override
   void didChangeDependencies() {
     super.didChangeDependencies();
-    final arguments =
-        ModalRoute.of(context)!.settings.arguments;
-    movieId = arguments is int
-        ? arguments.toString()
-        : arguments as String;
+    final arguments = ModalRoute.of(context)!.settings.arguments;
+    movieId = arguments is int ? arguments.toString() : arguments as String;
   }
 
   Widget build(BuildContext context) {
@@ -37,14 +34,11 @@ class _DetailsPageState extends State<DetailsPage> {
         title: FutureBuilder(
           future: ApiManager.getDetails(movieId!),
           builder: (context, snapshot) {
-            if (snapshot.connectionState ==
-                ConnectionState.waiting) {
-              return Center(
-                  child: CircularProgressIndicator());
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(child: CircularProgressIndicator());
             }
             if (snapshot.hasError) {
-              return Center(
-                  child: Text("Something Went Wrong!"));
+              return Center(child: Text("Something Went Wrong!"));
             }
             var movieDetail = snapshot.data;
             return Padding(
@@ -66,69 +60,50 @@ class _DetailsPageState extends State<DetailsPage> {
       body: FutureBuilder(
         future: ApiManager.getDetails(movieId!),
         builder: (context, snapshot) {
-          if (snapshot.connectionState ==
-              ConnectionState.waiting) {
-            return Center(
-                child: CircularProgressIndicator());
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
           }
           if (snapshot.hasError) {
-            return Center(
-                child: Text("Something Went Wrong!"));
+            return Center(child: Text("Something Went Wrong!"));
           }
           var movieDetail = snapshot.data;
+          print("$movieId");
           return Padding(
             padding: const EdgeInsets.all(0.1),
             child: Container(
               height: MediaQuery.of(context).size.height,
               child: Column(
-                crossAxisAlignment:
-                    CrossAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
                 mainAxisAlignment: MainAxisAlignment.start,
                 children: [
                   Stack(
                     children: [
                       CachedNetworkImage(
                         imageUrl: Constants.IMAGE_BASE_URL +
-                            (movieDetail?.backdropPath ??
-                                ""),
-                        height: MediaQuery.of(context)
-                                .size
-                                .height *
-                            0.235,
-                        width: MediaQuery.of(context)
-                                .size
-                                .width *
-                            1,
+                            (movieDetail?.backdropPath ?? ""),
+                        height: MediaQuery.of(context).size.height * 0.235,
+                        width: MediaQuery.of(context).size.width * 1,
                         fit: BoxFit.fitWidth,
-                        progressIndicatorBuilder: (context,
-                                url, downloadProgress) =>
-                            Center(
-                                child:
-                                    CircularProgressIndicator(
-                                        value:
-                                            downloadProgress
-                                                .progress)),
-                        errorWidget:
-                            (context, url, error) =>
-                                Icon(Icons.error),
+                        progressIndicatorBuilder:
+                            (context, url, downloadProgress) => Center(
+                                child: CircularProgressIndicator(
+                                    value: downloadProgress.progress)),
+                        errorWidget: (context, url, error) => Icon(Icons.error),
                       ),
                       Padding(
-                        padding:
-                            const EdgeInsets.only(top: 55),
+                        padding: const EdgeInsets.only(top: 55),
                         child: Center(
                           child: IconButton(
                             onPressed: () {
                               Navigator.pushNamed(
                                 context,
-                                DetailsVideoPlayer
-                                    .routeName,
+                                DetailsVideoPlayer.routeName,
                                 arguments:
                                     movieId, // Pass the movieId to the DetailsVideoPlayer page
                               );
                             },
                             icon: Icon(
-                              CupertinoIcons
-                                  .play_circle_fill,
+                              CupertinoIcons.play_circle_fill,
                               color: Colors.white,
                               size: 80,
                             ),
@@ -137,11 +112,7 @@ class _DetailsPageState extends State<DetailsPage> {
                       ),
                     ],
                   ),
-                  SizedBox(
-                      height: MediaQuery.of(context)
-                              .size
-                              .height *
-                          0.005),
+                  SizedBox(height: MediaQuery.of(context).size.height * 0.005),
                   Row(
                     children: [
                       Text(
@@ -166,40 +137,25 @@ class _DetailsPageState extends State<DetailsPage> {
                       ),
                     ],
                   ),
-                  SizedBox(
-                      height: MediaQuery.of(context)
-                              .size
-                              .height *
-                          0.005),
+                  SizedBox(height: MediaQuery.of(context).size.height * 0.005),
                   Stack(
                     children: [
                       ClipRRect(
-                        borderRadius:
-                            BorderRadius.circular(12),
+                        borderRadius: BorderRadius.circular(12),
                         child: Container(
-                          width: MediaQuery.of(context)
-                                  .size
-                                  .width *
-                              0.4,
+                          width: MediaQuery.of(context).size.width * 0.4,
                           child: Stack(
                             children: [
                               CachedNetworkImage(
-                                imageUrl: Constants
-                                        .IMAGE_BASE_URL +
-                                    (movieDetail
-                                            ?.posterPath ??
-                                        ""),
+                                imageUrl: Constants.IMAGE_BASE_URL +
+                                    (movieDetail?.posterPath ?? ""),
                                 fit: BoxFit.cover,
-                                progressIndicatorBuilder: (context,
-                                        url,
-                                        downloadProgress) =>
-                                    Center(
+                                progressIndicatorBuilder:
+                                    (context, url, downloadProgress) => Center(
                                         child: CircularProgressIndicator(
-                                            value: downloadProgress
-                                                .progress)),
-                                errorWidget:
-                                    (context, url, error) =>
-                                        Icon(Icons.error),
+                                            value: downloadProgress.progress)),
+                                errorWidget: (context, url, error) =>
+                                    Icon(Icons.error),
                               ),
                               // MyBookmarkWidget(
                               //   moviesList: movieDetail,
@@ -209,42 +165,29 @@ class _DetailsPageState extends State<DetailsPage> {
                         ),
                       ),
                       Padding(
-                        padding: const EdgeInsets.only(
-                            left: 170.0),
+                        padding: const EdgeInsets.only(left: 170.0),
                         child: Column(
-                          crossAxisAlignment:
-                              CrossAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
                             Wrap(
-                              spacing:
-                                  10, // gap between adjacent chips
-                              runSpacing:
-                                  7, // gap between lines
-                              children: movieDetail?.genres
-                                      ?.map((genre) {
+                              spacing: 10, // gap between adjacent chips
+                              runSpacing: 7, // gap between lines
+                              children: movieDetail?.genres?.map((genre) {
                                     return Container(
                                       decoration: BoxDecoration(
                                           borderRadius:
-                                              BorderRadius
-                                                  .circular(
-                                                      12),
+                                              BorderRadius.circular(12),
                                           border: Border.all(
-                                              color: Color(
-                                                  0xFF514F4F),
+                                              color: Color(0xFF514F4F),
                                               width: 1)),
                                       child: Padding(
-                                        padding:
-                                            const EdgeInsets
-                                                .all(13.0),
+                                        padding: const EdgeInsets.all(13.0),
                                         child: Text(
                                           genre.name ?? '',
                                           style: TextStyle(
-                                              color: Colors
-                                                  .white,
+                                              color: Colors.white,
                                               fontSize: 10,
-                                              fontWeight:
-                                                  FontWeight
-                                                      .w400),
+                                              fontWeight: FontWeight.w400),
                                         ),
                                       ),
                                     );
@@ -253,25 +196,18 @@ class _DetailsPageState extends State<DetailsPage> {
                             ),
                             SizedBox(
                                 height:
-                                    MediaQuery.of(context)
-                                            .size
-                                            .height *
-                                        0.009),
+                                    MediaQuery.of(context).size.height * 0.009),
                             Text(
                               maxLines: 6,
                               movieDetail?.overview ?? "",
                               style: TextStyle(
                                   color: Colors.white,
                                   fontSize: 13,
-                                  fontWeight:
-                                      FontWeight.w400),
+                                  fontWeight: FontWeight.w400),
                             ),
                             SizedBox(
                                 height:
-                                    MediaQuery.of(context)
-                                            .size
-                                            .height *
-                                        0.009),
+                                    MediaQuery.of(context).size.height * 0.009),
                             Row(
                               children: [
                                 Icon(
@@ -281,12 +217,10 @@ class _DetailsPageState extends State<DetailsPage> {
                                 ),
                                 Text(
                                   movieDetail?.voteAverage
-                                          ?.toStringAsFixed(
-                                              1) ??
+                                          ?.toStringAsFixed(1) ??
                                       "",
                                   style: TextStyle(
-                                      fontWeight:
-                                          FontWeight.w400,
+                                      fontWeight: FontWeight.w400,
                                       fontSize: 18,
                                       color: Colors.white),
                                 ),
@@ -297,17 +231,10 @@ class _DetailsPageState extends State<DetailsPage> {
                       ),
                     ],
                   ),
-                  SizedBox(
-                      height: MediaQuery.of(context)
-                              .size
-                              .height *
-                          0.005),
+                  SizedBox(height: MediaQuery.of(context).size.height * 0.005),
                   Container(
-                    height:
-                        MediaQuery.of(context).size.height *
-                            0.26,
-                    width:
-                        MediaQuery.of(context).size.width,
+                    height: MediaQuery.of(context).size.height * 0.26,
+                    width: MediaQuery.of(context).size.width,
                     color: Colors.grey,
                     child: DetailedContainerList(),
                   )
